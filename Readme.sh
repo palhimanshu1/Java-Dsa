ee2967efec1b0ae89c2a6e17bd09a601552f5f73
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-10 19:09:28 +0530

    Update Readme.sh

--- Readme.sh
+++ Readme.sh
@@ -1,1 +1,1 @@
-Visit Every Chapter Of dsa java coding problem solution. All this code solution is from Scalar Academy faculty Sumeet malik Sir
+Visit Every Chapter (Different branch created for every Chapter) Of dsa java coding problem solution. All this code solution is from Scalar Academy faculty Sumeet malik Sir
