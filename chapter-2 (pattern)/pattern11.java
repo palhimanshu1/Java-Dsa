d801b0b13259a7d98e9cc16dd2a6b427618b4861
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 00:40:41 +0530

    Update pattern11.java

--- chapter-2 (pattern)/pattern11.java
+++ chapter-2 (pattern)/pattern11.java
@@ -1,3 +1,9 @@
+// 1
+// 2       3
+// 4       5       6
+// 7       8       9       10
+// 11      12      13      14      15
+
 import java.util.*;
 public class pattern11 {
     public static void main(String[] args) {
