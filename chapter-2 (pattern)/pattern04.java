12304bf2bd168303b0794234292b3f4bfbeb8787
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 00:32:06 +0530

    Update and rename patter4.java to pattern04.java

--- chapter-2 (pattern)/pattern04.java
+++ chapter-2 (pattern)/pattern04.java
@@ -1,3 +1,9 @@
+*       *       *       *       *
+        *       *       *       *
+                *       *       *
+                        *       *
+                                *
+
 import java.util.*;
 public class patter4 {
     public static void main(String[] args) {
