0ac0de9b6e39fa27811a7caecaa27744643e91dd
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 00:28:14 +0530

    Update pattern13.java

--- chapter-2 (pattern)/pattern13.java
+++ chapter-2 (pattern)/pattern13.java
@@ -1,3 +1,12 @@
+// 5
+// 1
+// 1       1
+// 1       2       1
+// 1       3       3       1
+// 1       4       6       4       1
+
+
+
 import java.util.*;
 public class pattern13 {
     public static void main(String[] args) {
