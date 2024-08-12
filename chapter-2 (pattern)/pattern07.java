fbe6796c39208dbf8b43f4f6df1938bb25b02e3b
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 00:37:02 +0530

    Update and rename pattern7.java to pattern07.java

--- chapter-2 (pattern)/pattern07.java
+++ chapter-2 (pattern)/pattern07.java
@@ -1,3 +1,10 @@
+// *
+//         *
+//                 *
+//                         *
+//                                 *
+
+
 import java.util.*;;
 public class pattern7 {
     public static void main(String[] args) {
