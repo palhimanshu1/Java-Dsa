c1122865ca3e1b19fa936f76b6f9a1a08de5bfc1
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 00:42:07 +0530

    Update pattern12.java

--- chapter-2 (pattern)/pattern12.java
+++ chapter-2 (pattern)/pattern12.java
@@ -1,3 +1,11 @@
+0
+1       1
+2       3       5
+8       13      21      34
+55      89      144     233     377
+
+    //fibonacci series in triangle
+
 import java.util.*;
 
 public class pattern12 {
