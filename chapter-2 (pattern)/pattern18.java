502b250b84a15728d68ea1f36c9da21eaa01d8fb
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 17:18:12 +0530

    Add files via upload

---
+++ chapter-2 (pattern)/pattern18.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class pattern18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int space =0;
+        int star=n;
+        for(int i=1;i<=n;i++){
+
+
+            for(int j=1;j<=space;j++){
+                System.out.print("\t");
+            }
+
+
+            for(int j =1;j<=star;j++){
+                if(i>1 && i<=n/2 && j>1 && j<star){
+                    System.out.print("\t");
+                }else{
+                    System.out.print("*\t");
+                }
+                
+            }
+
+            
+
+            if(i<=n/2){
+                star-=2;
+                space++;
+            }else{
+                star+=2;
+                space--;
+            }
+            System.out.println();
+
+        }
+        sc.close();
+    }
+}
