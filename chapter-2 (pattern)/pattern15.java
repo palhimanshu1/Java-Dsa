6c0c2a034c27caaf1b045d50fd6e11adcd03e75d
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 17:11:47 +0530

    Add files via upload

---
+++ chapter-2 (pattern)/pattern15.java
@@ -0,0 +1,41 @@
+import java.util.*;
+;
+public class pattern15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sp=n/2;
+        int star=1;
+        int value=1;
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=sp;j++){
+                System.out.print("\t");
+            }
+
+            int cval=value;
+            for(int j=1;j<=star;j++){
+                System.out.print(cval+"\t");
+
+                if(j<=star/2){
+                    cval++;
+                }else{
+                    cval--;
+                }
+            }
+            if(i<=n/2){
+                sp--;
+                star+=2;
+                value++;
+            }else{
+                sp++;
+                star-=2;
+                value--;
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+    
+}
