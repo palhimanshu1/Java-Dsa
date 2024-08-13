98c65dad2ad5d2e89cd62984af8789ddf0e5871f
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 17:11:24 +0530

    Update pattern14.java

--- chapter-2 (pattern)/pattern14.java
+++ chapter-2 (pattern)/pattern14.java
@@ -1,3 +1,17 @@
+// 5 * 1 = 5
+// 5 * 2 = 10
+// 5 * 3 = 15
+// 5 * 4 = 20
+// 5 * 5 = 25
+// 5 * 6 = 30
+// 5 * 7 = 35
+// 5 * 8 = 40
+// 5 * 9 = 45
+// 5 * 10 = 50
+
+// printing table of any number upto 10
+
+
 import java.util.*;
 
 public class pattern14 {
