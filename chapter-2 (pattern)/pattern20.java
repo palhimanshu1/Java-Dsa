463f211e446ac3bb48edd207b6b00dc56c6584c8
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 17:21:15 +0530

    Add files via upload

---
+++ chapter-2 (pattern)/pattern20.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class pattern20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==1 ||j==n){
+                    System.out.print("*\t");
+                }else if(i>n/2 && (i==j || i+j==n+1)){
+                    System.out.print("*\t");
+
+                }else{
+                    System.out.print("\t");
+                }
+            } 
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
