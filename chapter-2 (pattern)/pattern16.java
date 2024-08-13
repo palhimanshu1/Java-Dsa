fbb94fa5cd7bb101aa705f32efa03cd1bee3b114
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 17:15:04 +0530

    Update pattern16.java

--- chapter-2 (pattern)/pattern16.java
+++ chapter-2 (pattern)/pattern16.java
@@ -1,3 +1,9 @@
+// 1                                                               1
+// 1       2                                               2       1
+// 1       2       3                               3       2       1
+// 1       2       3       4               4       3       2       1
+// 1       2       3       4       5       4       3       2       1
+
 import java.util.*;
 
 public class pattern16{
