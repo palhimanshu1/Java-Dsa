161c950df4b131c9c91e4617ead0b4069d16c5c0
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-13 17:13:56 +0530

    Add files via upload

---
+++ chapter-2 (pattern)/pattern16.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class pattern16{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int star=1;
+        int space=2*n-3;
+        
+        for(int i=1;i<=n;i++){
+            int value= 1;
+            for(int j=1;j<=star;j++){
+                System.out.print(value+"\t");
+                value++;
+            }
+            for(int j=1;j<=space;j++){
+                System.out.print("\t");
+            }
+            if(i==n){
+                star--;
+                value--;
+            }
+            for(int j=1;j<=star;j++){
+                value--;
+                System.out.print(value+"\t");
+                // value--;
+            }
+
+            star++;
+            space-=2;
+            System.out.println();
+
+
+        }
+        sc.close();
+    }
+}
