b9853e610e84fe5cb5e2b32bde2796c7c858da83
Himanshu pal <135658249+palhimanshu1@users.noreply.github.com>
2024-08-15 00:33:24 +0530

    Create BarChart.java
    Give first input n size of array. enter n numbers for array. get the printed pattern of star with height of value of array.

---
+++ BarChart.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class BarChart{
+    public static void main(String[] args) {
+        Scanner sc =   new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int max=arr[0];
+        for(int i=-0;i<arr.length;i++){
+            if(arr[i]>max){
+                max= arr[i];
+            }
+        }
+
+        for(int floor =max;floor>=1;floor--){
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]>=floor){
+                    System.out.print("*\t");
+                }else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
