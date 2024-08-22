751c63c7c533716ee484e492b075d79db040d95d
Himanshu <hmanshupal3024@gmail.com>
2024-08-22 19:56:39 +0530

    introduction basic input and output code

---
+++ intro.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class intro {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
