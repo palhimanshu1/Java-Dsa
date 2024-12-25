f96e995db370efa4b79f50b111b18400206b59ef
Himanshu <hmanshupal3024@gmail.com>
2024-12-25 09:58:26 +0530

    increasing decreasing order

--- PrintIncreasingDecreasing.java
+++ PrintIncreasingDecreasing.java
@@ -1,21 +1,19 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class PrintIncreasingDecreasing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        printDecreaingIncresingOrder(n, 1);
+        printIncreasingDecreasingOrder(n, 1);
         sc.close();
     }
 
-    public static void printDecreaingIncresingOrder(int n, int current) {
-        if (n > current) {
+    public static void printIncreasingDecreasingOrder(int n, int current) {
+        if (current > n) {
             return;
         }
-        System.out.println(current);
-        printDecreaingIncresingOrder(n, current + 1);
-        System.out.println(current);
-
+        System.out.print(current);
+        printIncreasingDecreasingOrder(n, current + 1);
+        System.out.print(current);
     }
-
 }
