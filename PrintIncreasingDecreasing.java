b64e87adb658dc1045eed88e3ccb1be967780202
Himanshu <hmanshupal3024@gmail.com>
2024-12-25 09:51:48 +0530

    file name changes

--- PrintIncreasingDecreasing.java
+++ PrintIncreasingDecreasing.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class incdec {
+public class PrintIncreasingDecreasing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
