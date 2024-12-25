d433f1190319e2196bab427826fe63198cdfc115
Himanshu <hmanshupal3024@gmail.com>
2024-12-25 09:49:29 +0530

    incresing decreasing order

---
+++ incdec.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class incdec {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printDecreaingIncresingOrder(n, 1);
+        sc.close();
+    }
+
+    public static void printDecreaingIncresingOrder(int n, int current) {
+        if (n > current) {
+            return;
+        }
+        System.out.println(current);
+        printDecreaingIncresingOrder(n, current + 1);
+        System.out.println(current);
+
+    }
+
+}
